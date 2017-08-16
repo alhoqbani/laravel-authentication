adfb1dc9a9551e23ab4e195f41c2e3d20788ea56
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-08-16 04:07:05 -0700

    Add post route to handle resending the activation email

--- app/Http/Controllers/Auth/ActivationController.php
+++ app/Http/Controllers/Auth/ActivationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Events\Auth\UserRequestedActivationToken;
 use App\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -27,4 +28,30 @@ class ActivationController extends Controller
     {
         return view('auth.activation.resend');
     }
+    
+    public function send(Request $request)
+    {
+        $this->validateEmail($request);
+    
+        $user = User::where('email', $request->email)->first();
+        
+        if ($user) {
+            event(new UserRequestedActivationToken($user));
+        }
+        
+        return redirect()->route('login')
+            ->withSuccess('Thank you. If we have your email in our record, we\'ll resend you the activation email');
+    }
+    
+    /**
+     * Validate the email for the given request.
+     *
+     * @param \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateEmail(Request $request)
+    {
+        $this->validate($request, ['email' => 'required|email']);
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 Route::get('auth/activate', 'Auth\ActivationController@activate')->name('auth.activate');
 Route::get('auth/resend', 'Auth\ActivationController@resend')->name('auth.resend');
+Route::post('auth/resend', 'Auth\ActivationController@send');
 
 Route::get('/', function () {
     return view('welcome');
