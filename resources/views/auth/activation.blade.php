1690dae7b992425b26742c6a49315cc2a21f8b84
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-08-16 03:42:51 -0700

    Update activation email to include email and activation token

--- resources/views/auth/activation.blade.php
+++ resources/views/auth/activation.blade.php
@@ -5,7 +5,11 @@ Dear {{$user->name}},
 
 Please click the link below to activate your account.
 
-@component('mail::button', ['url' => ''])
+@component('mail::button', ['url' =>
+    route('auth.activate', [
+        'token' => $user->activation_token,
+        'email' => $user->email
+    ])])
 Activate
 @endcomponent
 
