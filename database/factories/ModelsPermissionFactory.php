67441c8da81fef45111e2c93994f024df0600bd9
Hamoud Alhoqbani <h.alhoqbani@gmail.com>
2017-09-18 19:00:09 -0700

    update Models factories

--- database/factories/ModelsPermissionFactory.php
+++ database/factories/ModelsPermissionFactory.php
@@ -4,6 +4,9 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Models\Permission::class, function (Faker $faker) {
     return [
-        //
+        'role_id' => function() {
+            return factory(\App\User::class)->create()->id;
+        },
+        'title' => $faker->word
     ];
 });
